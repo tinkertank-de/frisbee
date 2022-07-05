a8d47e133f7fed58c262bc53aa53400f6a34e244
flogreth <florian.greth@tinkertank.de>
2022-07-05 13:52:19 +0200

    code.py is the new standard code
    with neopixel bee

--- Software/code.py
+++ Software/code.py
@@ -1,186 +1,176 @@
-# Hallo Welt
-# Hallo Welt 2
+## ---- TINKERTANK FRISBEE ---- ##
+## ------ standard code ------- ##
 
 ## ---- Imports ---- ##
-import board
-from digitalio import Pull
-from piper_blockly import *
 import time
+import board
+import digitalio
+import touchio
 import usb_hid
+import neopixel
 from adafruit_hid.keyboard import Keyboard
-from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 from adafruit_hid.keycode import Keycode
 from adafruit_hid.mouse import Mouse
-
-## ---- Definitions ---- ##
-LEFT_old = None
-SPACE_old = None
-RIGHT_old = None
-DOWN_old = None
-UP_old = None
-UP = None
-CLICK_old = None
-LEFT = None
-SPACE = None
-RIGHT = None
-DOWN = None
-CLICK = None
-GP11 = piperPin(board.GP11, "GP11")
-
-try:
-  set_digital_view(True)
-except:
-  pass
-
-time.sleep(1)
 keyboard_HID = Keyboard(usb_hid.devices)
-keyboard_HID_layout = KeyboardLayoutUS(keyboard_HID)
-
-GP14 = piperPin(board.GP14, "GP14")
-
-# LINKE PFEILTASTE
-
-def Key_LEFT_Service_Loop():
-  global LEFT_old, SPACE_old, RIGHT_old, DOWN_old, UP_old, UP, CLICK_old, LEFT, SPACE, RIGHT, DOWN, CLICK
-  LEFT_old = LEFT
-  print(GP11.checkPin(None))
-  if GP11.checkPin(None):
-    LEFT = 1
-  else:
-    LEFT = 0
-  time.sleep(0.01)
-  if LEFT > LEFT_old:
-    keyboard_HID.press(Keycode.LEFT_ARROW)
-    GP14.setPin(1)
-  elif LEFT < LEFT_old:
-    keyboard_HID.release(Keycode.LEFT_ARROW)
-    GP14.setPin(0)
-
-GP1 = piperPin(board.GP0, "GP0")
-GP1 = piperPin(board.GP1, "GP1")
-GP2 = piperPin(board.GP2, "GP2")
-GP26 = piperPin(board.GP26, "GP26")
-GP27 = piperPin(board.GP27, "GP27")
-GP28 = piperPin(board.GP28, "GP28")
-
-# Describe this function...
-def Key_SPACE_Service_Loop():
-  global LEFT_old, SPACE_old, RIGHT_old, DOWN_old, UP_old, UP, CLICK_old, LEFT, SPACE, RIGHT, DOWN, CLICK
-  SPACE_old = SPACE
-  if GP21.checkPin(None):
-    SPACE = 1
-  else:
-    SPACE = 0
-  time.sleep(0.01)
-  if SPACE > SPACE_old:
-    keyboard_HID.press(Keycode.SPACEBAR)
-    GP14.setPin(1)
-  elif SPACE < SPACE_old:
-    keyboard_HID.release(Keycode.SPACEBAR)
-    GP14.setPin(0)
-
-GP12 = piperPin(board.GP12, "GP12")
-# Describe this function...
-def Key_RIGHT_Service_Loop():
-  global LEFT_old, SPACE_old, RIGHT_old, DOWN_old, UP_old, UP, CLICK_old, LEFT, SPACE, RIGHT, DOWN, CLICK
-  RIGHT_old = RIGHT
-  if GP12.checkPin(None):
-    RIGHT = 1
-  else:
-    RIGHT = 0
-  time.sleep(0.01)
-  if RIGHT > RIGHT_old:
-    keyboard_HID.press(Keycode.RIGHT_ARROW)
-    GP14.setPin(1)
-  elif RIGHT < RIGHT_old:
-    keyboard_HID.release(Keycode.RIGHT_ARROW)
-    GP14.setPin(0)
-
-GP13 = piperPin(board.GP13, "GP13")
-# Describe this function...
-def Key_DOWN_Service_Loop():
-  global LEFT_old, SPACE_old, RIGHT_old, DOWN_old, UP_old, UP, CLICK_old, LEFT, SPACE, RIGHT, DOWN, CLICK
-  DOWN_old = DOWN
-  if GP13.checkPin(None):
-    DOWN = 1
-  else:
-    DOWN = 0
-  time.sleep(0.01)
-  if DOWN > DOWN_old:
-    keyboard_HID.press(Keycode.DOWN_ARROW)
-    GP14.setPin(1)
-  elif DOWN < DOWN_old:
-    keyboard_HID.release(Keycode.DOWN_ARROW)
-    GP14.setPin(0)
-
-GP10 = piperPin(board.GP10, "GP10")
-# Describe this function...
-def Key_UP_Service_Loop():
-  global LEFT_old, SPACE_old, RIGHT_old, DOWN_old, UP_old, UP, CLICK_old, LEFT, SPACE, RIGHT, DOWN, CLICK
-  UP_old = UP
-  if GP10.checkPin(None):
-    UP = 1
-  else:
-    UP = 0
-  time.sleep(0.01)
-  if UP > UP_old:
-    keyboard_HID.press(Keycode.UP_ARROW)
-    GP14.setPin(1)
-  elif UP < UP_old:
-    keyboard_HID.release(Keycode.UP_ARROW)
-    GP14.setPin(0)
-
-# Describe this function...
-def all_Variables_to_0():
-  global LEFT_old, SPACE_old, RIGHT_old, DOWN_old, UP_old, UP, CLICK_old, LEFT, SPACE, RIGHT, DOWN, CLICK
-  UP = 0
-  UP_old = 0
-  CLICK = 0
-  CLICK_old = 0
-  DOWN = 0
-  DOWN_old = 0
-  LEFT = 0
-  LEFT_old = 0
-  RIGHT = 0
-  RIGHT_old = 0
-  SPACE = 0
-  SPACE_old = 0
-
-GP20 = piperPin(board.GP20, "GP20")
 mouse_HID = Mouse(usb_hid.devices)
 
-# Describe this function...
-def Mouse_CLICK_Service_Loop():
-  global LEFT_old, SPACE_old, RIGHT_old, DOWN_old, UP_old, UP, CLICK_old, LEFT, SPACE, RIGHT, DOWN, CLICK
-  CLICK_old = CLICK
-  if GP20.checkPin(None):
-    CLICK = 1
-  else:
-    CLICK = 0
-  time.sleep(0.01)
-  if CLICK > CLICK_old:
-    mouse_HID.press(Mouse.LEFT_BUTTON)
-    GP14.setPin(1)
-  elif CLICK < CLICK_old:
-    mouse_HID.release(Mouse.LEFT_BUTTON)
-    GP14.setPin(0)
+## ---- Definitions ---- ##
 
+pinValueOld = [0,0,0,0,0,0] # previous pinValues
+pinValue = [0,0,0,0,0,0]    # sum of pinValue Readings 
+numReadings = 10			# number of readings before evaluating
+threshold = 8    			# threshold to trigger key
+
+pixels = neopixel.NeoPixel(board.GP14, 1, brightness=1, auto_write=False)
+
+RED = (255, 0, 0)
+YELLOW = (255, 150, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 20, 255)
+BLACK = (0,0,0)
+pxColor = BLACK
+
+Pin10= digitalio.DigitalInOut(board.GP10)
+Pin10.direction=digitalio.Direction.INPUT
+
+Pin11= digitalio.DigitalInOut(board.GP11)
+Pin11.direction=digitalio.Direction.INPUT
+
+Pin12= digitalio.DigitalInOut(board.GP12)
+Pin12.direction=digitalio.Direction.INPUT
+
+Pin13= digitalio.DigitalInOut(board.GP13)
+Pin13.direction=digitalio.Direction.INPUT
+
+Pin20= digitalio.DigitalInOut(board.GP20)
+Pin20.direction=digitalio.Direction.INPUT
+
+Pin21= digitalio.DigitalInOut(board.GP21)
+Pin21.direction=digitalio.Direction.INPUT
+
+#touch
+touch_pad = board.GP16
+touch = touchio.TouchIn(touch_pad)
+
+# all GPIOs to output high #
+
+highPin0= digitalio.DigitalInOut(board.GP0)
+highPin0.direction=digitalio.Direction.OUTPUT
+highPin0.value = 1
+
+highPin1= digitalio.DigitalInOut(board.GP1)
+highPin1.direction=digitalio.Direction.OUTPUT
+highPin1.value = 1
+
+highPin2= digitalio.DigitalInOut(board.GP2)
+highPin2.direction=digitalio.Direction.OUTPUT
+highPin2.value = 1
+
+highPin3= digitalio.DigitalInOut(board.GP26)
+highPin3.direction=digitalio.Direction.OUTPUT
+highPin3.value = 1
+
+highPin4= digitalio.DigitalInOut(board.GP27)
+highPin4.direction=digitalio.Direction.OUTPUT
+highPin4.value = 1
+
+highPin5= digitalio.DigitalInOut(board.GP28)
+highPin5.direction=digitalio.Direction.OUTPUT
+highPin5.value = 1
+
+## functions ##
+
+def sensePin(pin):    # digital Read pins and add to 'pinValue' variable
+    global pinValue  
+    if pin == 0:                      
+        pinValue[0] += Pin10.value
+    elif pin == 1:
+        pinValue[1] += Pin11.value
+    elif pin == 2:
+        pinValue[2] += Pin12.value
+    elif pin == 3:
+        pinValue[3] += Pin13.value
+    elif pin == 4:
+        pinValue[4] += Pin20.value
+    elif pin == 5:
+        pinValue[5] += Pin21.value
+
+def evaluatePins():             # evaluate 'pinValue'
+	for i in range(6):
+		global pinValue, pinValueOld, pxColor 
+		
+		# print (pinValue[i], end=" ")    	# print raw sum values for debugging
+		if pinValue[i] > threshold:			# see if pinValue is higher than threshold
+			pinValue[i] = 1
+			if i < 4: pxColor = GREEN		#arrow keys: green
+			elif i == 4: pxColor = RED
+			elif i == 5: pxColor = BLUE
+			
+		else:
+			pinValue[i] = 0					# not pressed
+			
+		if pinValueOld[i] < pinValue[i]:	
+			if i == 0: keyboard_HID.press(Keycode.UP_ARROW)
+			elif i == 1: keyboard_HID.press(Keycode.LEFT_ARROW)
+			elif i == 2: keyboard_HID.press(Keycode.RIGHT_ARROW)
+			elif i == 3: keyboard_HID.press(Keycode.DOWN_ARROW)
+			elif i == 4: mouse_HID.press(Mouse.LEFT_BUTTON)
+			elif i == 5: keyboard_HID.press(Keycode.SPACEBAR)
+			
+		elif pinValueOld[i] > pinValue[i]:
+			if i == 0: keyboard_HID.release(Keycode.UP_ARROW)
+			elif i == 1: keyboard_HID.release(Keycode.LEFT_ARROW)
+			elif i == 2: keyboard_HID.release(Keycode.RIGHT_ARROW)
+			elif i == 3: keyboard_HID.release(Keycode.DOWN_ARROW)
+			elif i == 4: mouse_HID.release(Mouse.LEFT_BUTTON)
+			elif i == 5: keyboard_HID.release(Keycode.SPACEBAR)
+
+		pinValueOld[i] = pinValue[i]		# save previous value
+		pinValue[i] = 0   					# reset RawValue
+	
+def readTouchPoint():
+    global touch, pxColor
+    if touch.value:
+        print ("touched")
+        pxColor =(YELLOW)
+
+## startup animation
+
+for k in range(80):
+    pixels[0] = (k,k,0)
+    time.sleep(0.01)
+    pixels.show()
+	
+pixels[0] = (10,10,0)
+pixels.show()
+time.sleep(0.1)
+pixels[0] = (80,80,0)
+pixels.show()
+time.sleep(0.1)
+pixels[0] = (20,20,0)
+pixels.show()
+time.sleep(0.1)
+pixels[0] = YELLOW
+pixels.show()
+time.sleep(1)
+for k in range(0, 255):
+    pixels[0] = (255-k,255-k,0)
+    time.sleep(0.001)
+    pixels.show()
 
 ## ---- Code ---- ##
-all_Variables_to_0()
-GP1.setPin(1)
-GP2.setPin(1)
-GP3.setPin(1)
-GP4.setPin(1)
-GP5.setPin(1)
-GP6.setPin(1)
-GP7.setPin(1)
 while True:
-  Key_UP_Service_Loop()
-  Key_DOWN_Service_Loop()
-  Key_LEFT_Service_Loop()
-  Key_RIGHT_Service_Loop()
-  Key_SPACE_Service_Loop()
-  Mouse_CLICK_Service_Loop()
+    pxColor = BLACK
+    
+    for i in range(numReadings):	#read [10] times
+		for j in range(6):	
+			sensePin(j)		# read all 6 Pins
+		time.sleep(0.002)
 
-  time.sleep(0.01)
+    evaluatePins()
+    readTouchPoint()
+
+    pixels[0] = pxColor
+    pixels.show()
+    
+    # print('')   #for debugging
